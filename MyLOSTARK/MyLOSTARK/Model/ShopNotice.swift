d060bfe8c2763cdcf5a6a21d4e1b6de38361c479
Hugh <102569735+Hugh-github@users.noreply.github.com>
2023-07-14 15:02:07 +0900

    feat: Main화면에서 사용하는 Model 구성

---
+++ MyLOSTARK/MyLOSTARK/Model/ShopNotice.swift
@@ -0,0 +1,19 @@
+//
+//  ShopNoti.swift
+//  MyLOSTARK
+//
+//  Created by dhoney96 on 2023/07/14.
+//
+
+
+struct ShopNotice: Decodable {
+    let title: String
+    let date: String
+    let link: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "Title"
+        case date = "Date"
+        case link = "Link"
+    }
+}
