6544aa97e6b6c7c52a1ff4d7a051e69c5867492d
Hugh <102569735+Hugh-github@users.noreply.github.com>
2023-07-20 15:10:18 +0900

    feat : Section HeaderView 구현

---
+++ MyLOSTARK/MyLOSTARK/CommonHeaderView.swift
@@ -0,0 +1,43 @@
+//
+//  CommonHeaderView.swift
+//  MyLOSTARK
+//
+//  Created by dhoney96 on 2023/07/20.
+//
+
+import UIKit
+
+class CommonHeaderView: UIView {
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.backgroundColor = .systemBackground
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHeader(title: String, color: UIColor) {
+        self.titleLabel.text = title
+        self.backgroundColor = color
+    }
+    
+    private func setLayout() {
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5)
+        ])
+    }
+}
