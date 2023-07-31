758185047b657407aed57ecd6c51356c25c16d60
Hugh <102569735+Hugh-github@users.noreply.github.com>
2023-07-31 16:03:12 +0900

    refact : MainCollectionView 타입 제거 및 Controller에 CollectionView 코드 추가

--- MyLOSTARK/MyLOSTARK/View/Main/MainViewController.swift
+++ MyLOSTARK/MyLOSTARK/View/Main/MainViewController.swift
@@ -7,31 +7,28 @@
 
 import UIKit
 
-enum MainViewSection: Int, CaseIterable {
-    case calendar = 0
-    case characterBookmark
-    case shopNotice
-    case event
-}
-
 class MainViewController: UIViewController {
     typealias DataSource = UICollectionViewDiffableDataSource<MainViewSection, AnyHashable>
     typealias Snapshot = NSDiffableDataSourceSnapshot<MainViewSection, AnyHashable>
     
-    private let mainView = MainCollectionView()
-    private let viewModel = MainViewModel()
-    private var dataSource: DataSource!
-    
-    override func loadView() {
-        mainView.collectionView.delegate = self
-        self.view = mainView
+    enum MainViewSection: CaseIterable {
+        case calendar
+        case characterBookmark
+        case shopNotice
+        case event
     }
+    
+    private var collectionView: UICollectionView! = nil
+    private var dataSource: DataSource! = nil
+    
+    private let viewModel = MainViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         
         self.setNavigationItem()
+        self.configureCollectionView()
         self.createCell()
         self.createHeaderView()
         self.initailSnapshot()
@@ -39,38 +36,9 @@ class MainViewController: UIViewController {
         
         self.viewModel.fetchData()
     }
-    
-    private func createCell() {
-        let calendarRegistration = createCalendarSectionCell()
-        let shopNoticeRegistration = createShopNoticeSectionCell()
-        let eventRegistration = createEventSectionCell()
-        
-        self.dataSource = DataSource(collectionView: self.mainView.collectionView) { (collectionView, indexPath, itemIdentifier) in            
-            if let item = itemIdentifier as? Contents {
-                return collectionView.dequeueConfiguredReusableCell(
-                    using: calendarRegistration,
-                    for: indexPath,
-                    item: item
-                )
-            } else if let item = itemIdentifier as? ShopNotice {
-                return collectionView.dequeueConfiguredReusableCell(
-                    using: shopNoticeRegistration,
-                    for: indexPath,
-                    item: item
-                )
-            } else if let item = itemIdentifier as? Event {
-                return collectionView.dequeueConfiguredReusableCell(
-                    using: eventRegistration,
-                    for: indexPath,
-                    item: item
-                )
-            }
-            
-            return UICollectionViewCell()
-        }
-    }
 }
 
+// MARK: CollectionView Delegate
 extension MainViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if indexPath.section == 0 {
@@ -99,57 +67,139 @@ extension MainViewController: UICollectionViewDelegate {
     }
 }
 
-// MARK: Snapshot
+// MARK: Configure CollectionView
 extension MainViewController {
-    private func initailSnapshot() {
-        var snapshot = Snapshot()
+    private func configureCollectionView() {
+        self.collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: setSectionLayout())
+        self.collectionView.delegate = self
+        self.collectionView.backgroundColor = .systemGray6
+        self.collectionView.register(
+            CommonHeaderView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: "Header"
+        )
         
-        snapshot.appendSections(MainViewSection.allCases)
-        self.dataSource.apply(snapshot)
+        view.addSubview(collectionView)
     }
     
-    private func subscribeViewModel() {
-        self.subscribeContent()
-        self.subscribeShopNotice()
-        self.subscribeEvent()
-    }
-    
-    private func subscribeContent() {
-        var sectionSnapshot = NSDiffableDataSourceSectionSnapshot<AnyHashable>()
-        
-        self.viewModel.subscribeContents(on: self) { contents in
-            DispatchQueue.main.async {
-                sectionSnapshot.append(contents)
-                self.dataSource.apply(sectionSnapshot, to: .calendar)
+    private func setSectionLayout() -> UICollectionViewLayout {
+        let sectionProvider = { [weak self] (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            guard let self = self else { return nil }
+            let sectionKind = dataSource.sectionIdentifier(for: sectionIndex)
+            
+            switch sectionKind {
+            case .calendar:
+                return setCalendarLayout()
+            case .characterBookmark:
+                return setBookmarkLayout()
+            case .shopNotice:
+                return setShopNoticeLayout(environment: layoutEnvironment)
+            case .event:
+                return setEventLayout()
+            case .none:
+                return nil
             }
         }
+        
+        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        configuration.interSectionSpacing = 15
+        
+        return UICollectionViewCompositionalLayout(sectionProvider: sectionProvider, configuration: configuration)
     }
     
-    private func subscribeShopNotice() {
-        var sectionSnapshot = NSDiffableDataSourceSectionSnapshot<AnyHashable>()
+    private func setCalendarLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.33), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
         
-        self.viewModel.subscribeShopNotice(on: self) { notice in
-            DispatchQueue.main.async {
-                sectionSnapshot.append(notice)
-                self.dataSource.apply(sectionSnapshot, to: .shopNotice)
-            }
-        }
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.22))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.boundarySupplementaryItems = [self.getHeader()]
+        
+        return section
     }
     
-    private func subscribeEvent() {
-        var sectionSnapshot = NSDiffableDataSourceSectionSnapshot<AnyHashable>()
+    private func setBookmarkLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
         
-        self.viewModel.subscribeEvent(on: self) { events in
-            DispatchQueue.main.async {
-                sectionSnapshot.append(events)
-                self.dataSource.apply(sectionSnapshot, to: .event)
-            }
-        }
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.2))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.boundarySupplementaryItems = [self.getHeader()]
+        
+        return section
+    }
+    
+    private func setShopNoticeLayout(environment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection {
+        let listConfiguration = UICollectionLayoutListConfiguration(appearance: .plain)
+        let section = NSCollectionLayoutSection.list(using: listConfiguration, layoutEnvironment: environment)
+        section.boundarySupplementaryItems = [self.getHeader()]
+        
+        return section
+    }
+    
+    private func setEventLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.7), heightDimension: .fractionalHeight(0.22))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        section.boundarySupplementaryItems = [self.getHeader()]
+        
+        return section
+    }
+    
+    private func getHeader() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.05))
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: UICollectionView.elementKindSectionHeader,
+            alignment: .top
+        )
+        
+        return header
     }
 }
 
-// MARK: Create Cell & Header
+// MARK: Configure DataSource
 extension MainViewController {
+    private func createCell() {
+        let calendarRegistration = createCalendarSectionCell()
+        let shopNoticeRegistration = createShopNoticeSectionCell()
+        let eventRegistration = createEventSectionCell()
+        
+        self.dataSource = DataSource(collectionView: self.collectionView) { (collectionView, indexPath, itemIdentifier) in
+            if let item = itemIdentifier as? Contents {
+                return collectionView.dequeueConfiguredReusableCell(
+                    using: calendarRegistration,
+                    for: indexPath,
+                    item: item
+                )
+            } else if let item = itemIdentifier as? ShopNotice {
+                return collectionView.dequeueConfiguredReusableCell(
+                    using: shopNoticeRegistration,
+                    for: indexPath,
+                    item: item
+                )
+            } else if let item = itemIdentifier as? Event {
+                return collectionView.dequeueConfiguredReusableCell(
+                    using: eventRegistration,
+                    for: indexPath,
+                    item: item
+                )
+            }
+            
+            return UICollectionViewCell()
+        }
+    }
+    
     private func createHeaderView() {
         self.dataSource.supplementaryViewProvider = { (collectionView, elementKind, indexPath) -> UICollectionReusableView? in
             guard elementKind == UICollectionView.elementKindSectionHeader else {
@@ -210,6 +260,55 @@ extension MainViewController {
             var configuration = cell.defaultContentConfiguration()
             configuration.text = itemIdentifier.title
             cell.contentConfiguration = configuration
+        }
+    }
+}
+
+// MARK: Snapshot
+extension MainViewController {
+    private func initailSnapshot() {
+        var snapshot = Snapshot()
+        
+        snapshot.appendSections(MainViewSection.allCases)
+        self.dataSource.apply(snapshot)
+    }
+    
+    private func subscribeViewModel() {
+        self.subscribeContent()
+        self.subscribeShopNotice()
+        self.subscribeEvent()
+    }
+    
+    private func subscribeContent() {
+        var sectionSnapshot = NSDiffableDataSourceSectionSnapshot<AnyHashable>()
+        
+        self.viewModel.subscribeContents(on: self) { contents in
+            DispatchQueue.main.async {
+                sectionSnapshot.append(contents)
+                self.dataSource.apply(sectionSnapshot, to: .calendar)
+            }
+        }
+    }
+    
+    private func subscribeShopNotice() {
+        var sectionSnapshot = NSDiffableDataSourceSectionSnapshot<AnyHashable>()
+        
+        self.viewModel.subscribeShopNotice(on: self) { notice in
+            DispatchQueue.main.async {
+                sectionSnapshot.append(notice)
+                self.dataSource.apply(sectionSnapshot, to: .shopNotice)
+            }
+        }
+    }
+    
+    private func subscribeEvent() {
+        var sectionSnapshot = NSDiffableDataSourceSectionSnapshot<AnyHashable>()
+        
+        self.viewModel.subscribeEvent(on: self) { events in
+            DispatchQueue.main.async {
+                sectionSnapshot.append(events)
+                self.dataSource.apply(sectionSnapshot, to: .event)
+            }
         }
     }
 }
