580695c0d8f93b1dd2e21b649f2c90dc81b79027
Hugh <102569735+Hugh-github@users.noreply.github.com>
2023-08-04 11:55:34 +0900

    feat : BookmarkCell 구현 및 placeholder 이미지 추가

---
+++ MyLOSTARK/MyLOSTARK/View/Cell/Main/BookmarkCell.swift
@@ -0,0 +1,75 @@
+//
+//  BookmarkCell.swift
+//  MyLOSTARK
+//
+//  Created by dhoney96 on 2023/08/04.
+//
+
+import UIKit
+
+class BookmarkCell: UICollectionViewCell {
+    private let jobClassImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private let itemLevelLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubview() {
+        addSubview(jobClassImageView)
+        addSubview(itemLevelLabel)
+        addSubview(nameLabel)
+    }
+    
+    private func setLayout() {
+        NSLayoutConstraint.activate([
+            self.jobClassImageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            self.jobClassImageView.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            self.jobClassImageView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5)
+        ])
+
+        NSLayoutConstraint.activate([
+            self.itemLevelLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            self.itemLevelLabel.topAnchor.constraint(equalTo: jobClassImageView.bottomAnchor, constant: 10)
+        ])
+
+        NSLayoutConstraint.activate([
+            self.nameLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            self.nameLabel.topAnchor.constraint(equalTo: itemLevelLabel.bottomAnchor, constant: 10)
+        ])
+    }
+    
+    func setContent(image: UIImage?, level: String?, name: String?) {
+        self.jobClassImageView.image = image
+        self.itemLevelLabel.text = level
+        self.nameLabel.text = name
+    }
+}
