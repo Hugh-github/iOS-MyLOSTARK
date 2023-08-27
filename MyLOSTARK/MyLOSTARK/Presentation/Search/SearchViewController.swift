07dc163f25e5b63ad1acbe2c938af4b7d558cc52
Hugh <102569735+Hugh-github@users.noreply.github.com>
2023-08-27 14:05:57 +0900

    feat: SearchController 추가

--- MyLOSTARK/MyLOSTARK/Presentation/Search/SearchViewController.swift
+++ MyLOSTARK/MyLOSTARK/Presentation/Search/SearchViewController.swift
@@ -7,12 +7,43 @@
 
 import UIKit
 
+// Cell 커스텀 해야 한다.
+// 기존의 CommenHeaderView를 조금 수정하자.
 class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        self.configureNavigationBar()
     }
     
+}
+
+extension SearchViewController {
+    func configureNavigationBar() {
+        let searchController = UISearchController()
+        searchController.searchBar.barTintColor = .black
+        searchController.searchBar.tintColor = .black
+        searchController.searchBar.placeholder = "캐릭터 이름"
+        searchController.searchBar.setValue("취소", forKey: "cancelButtonText")
+        searchController.searchBar.showsCancelButton = false
+        searchController.searchBar.delegate = self
+        
+        navigationItem.searchController = searchController
+        navigationItem.title = "캐릭터 검색"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = nil
+        searchBar.setShowsCancelButton(false, animated: true)
+    }
 }
