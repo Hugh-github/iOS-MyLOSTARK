913743a8bf27f84357e364b33d1a6ae10b19956f
Hugh <102569735+Hugh-github@users.noreply.github.com>
2023-09-11 20:14:35 +0900

    feat : CoreDataStorage backgroundContext 및 task 추가

--- MyLOSTARK/MyLOSTARK/Data/CoreData/CoreDataStorage.swift
+++ MyLOSTARK/MyLOSTARK/Data/CoreData/CoreDataStorage.swift
@@ -7,6 +7,8 @@
 
 import CoreData
 
+// MARK: BackgroundViewContext에서 동작 하도록 코드를 작성해야 한다.
+
 final class CoreDataStorage {
     static let shared = CoreDataStorage()
     
@@ -23,6 +25,22 @@ final class CoreDataStorage {
     
     var viewContext: NSManagedObjectContext {
         return self.persistentContainer.viewContext
+    }
+    
+    var backgroundContext: NSManagedObjectContext {
+        let context = persistentContainer.newBackgroundContext() // mainContext의 Child Context를 사용한다는 의미이다.
+        context.automaticallyMergesChangesFromParent = true
+        return context
+    }
+    
+    func performContextTask(_ task: @escaping (NSManagedObjectContext) -> Void) {
+        task(viewContext)
+    }
+    
+    func performBackgroundTask(_ task: @escaping (NSManagedObjectContext) -> Void) {
+        backgroundContext.perform {
+            task(self.viewContext)
+        }
     }
     
     func saveContext() {
