39124742c54d9799e2673aa556502ed37213cf4b
Hugh <102569735+Hugh-github@users.noreply.github.com>
2023-09-16 16:11:07 +0900

    docs : RecentSearchRepository 파일 분리

--- MyLOSTARK/MyLOSTARK/Data/Repository/BookmarkRepository.swift
+++ MyLOSTARK/MyLOSTARK/Data/Repository/BookmarkRepository.swift
@@ -5,8 +5,6 @@
 //  Created by dhoney96 on 2023/09/13.
 //
 
-import Foundation
-
 class BookmarkRepository: DefualtRepository {
     typealias T = CharacterBookmark
     
@@ -33,44 +31,5 @@ class BookmarkRepository: DefualtRepository {
     
     func delete(_ character: CharacterBookmark) {
         self.bookmarkStorage.deleteBookmark(character.name)
-    }
-}
-
-class RecentSearchRepository: DefualtRepository {
-    typealias T = RecentCharacterInfo
-    
-    private let recentSearchStorage = CoreDataRecentSearchStorage()
-    
-    func fetch() async -> [RecentCharacterInfo] {
-        var searchList = [RecentCharacterInfo]()
-        
-        do {
-            let list = try await recentSearchStorage.fetchRecentSearch()
-            list.forEach { search in
-                searchList.append(search.toDomain())
-            }
-        } catch {
-            recentSearchStorage.rollBack()
-        }
-        
-        return searchList
-    }
-    
-    func create(_ search: RecentCharacterInfo) {
-        self.recentSearchStorage.createRecentSearch(search)
-    }
-    
-    func delete(_ search: RecentCharacterInfo) {
-        guard let name = search.name else { return }
-        
-        self.recentSearchStorage.deleteRecentSearch(name)
-    }
-    
-    func deleteAll() {
-        self.recentSearchStorage.deleteAllSearch()
-    }
-    
-    func update(_ search: RecentCharacterInfo) {
-        self.recentSearchStorage.updateResultSearch(search)
     }
 }
---
+++ MyLOSTARK/MyLOSTARK/Data/Repository/RecentSearchRepository.swift
@@ -0,0 +1,45 @@
+//
+//  RecentSearchRepository.swift
+//  MyLOSTARK
+//
+//  Created by dhoney96 on 2023/09/16.
+//
+
+class RecentSearchRepository: DefualtRepository {
+    typealias T = RecentCharacterInfo
+    
+    private let recentSearchStorage = CoreDataRecentSearchStorage()
+    
+    func fetch() async -> [RecentCharacterInfo] {
+        var searchList = [RecentCharacterInfo]()
+        
+        do {
+            let list = try await recentSearchStorage.fetchRecentSearch()
+            list.forEach { search in
+                searchList.append(search.toDomain())
+            }
+        } catch {
+            recentSearchStorage.rollBack()
+        }
+        
+        return searchList
+    }
+    
+    func create(_ search: RecentCharacterInfo) {
+        self.recentSearchStorage.createRecentSearch(search)
+    }
+    
+    func delete(_ search: RecentCharacterInfo) {
+        guard let name = search.name else { return }
+        
+        self.recentSearchStorage.deleteRecentSearch(name)
+    }
+    
+    func deleteAll() {
+        self.recentSearchStorage.deleteAllSearch()
+    }
+    
+    func update(_ search: RecentCharacterInfo) {
+        self.recentSearchStorage.updateResultSearch(search)
+    }
+}
