be2165888150f9b05d65030a7c7e1793d7ee94fb
Hugh <102569735+Hugh-github@users.noreply.github.com>
2023-09-21 20:23:18 +0900

    refact : Entity 객체에서 불 필요한 함수 제거
    - 최대한 Entity를 순수한 객체로 사용하기 위한 수정

--- MyLOSTARK/MyLOSTARK/Domain/Entities/CoreData/CharacterBookmark.swift
+++ MyLOSTARK/MyLOSTARK/Domain/Entities/CoreData/CharacterBookmark.swift
@@ -5,17 +5,8 @@
 //  Created by dhoney96 on 2023/08/03.
 //
 
-struct CharacterBookmark: Hashable {
+struct CharacterBookmark {
     var jobClass: String
     var itemLevel: String
     var name: String
-    
-    func toRecentSearch() -> RecentCharacterInfo {
-        return RecentCharacterInfo(
-            name: name,
-            jobClass: jobClass,
-            itemLevel: itemLevel,
-            isBookmark: false
-        )
-    }
 }
--- MyLOSTARK/MyLOSTARK/Domain/Entities/CoreData/RecentCharacterInfo.swift
+++ MyLOSTARK/MyLOSTARK/Domain/Entities/CoreData/RecentCharacterInfo.swift
@@ -10,9 +10,4 @@ struct RecentCharacterInfo {
     let jobClass: String?
     let itemLevel: String?
     let isBookmark: Bool
-    
-    // MARK: 해당 함수는 순수함수이다. -> SideEffect가 발생하지 않는다.
-    func toBookmark() -> CharacterBookmark {
-        return CharacterBookmark(jobClass: jobClass!, itemLevel: itemLevel!, name: name!)
-    }
 }
